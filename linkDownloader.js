1e5aaebc5258a6d565c8e5d08a36ef94d0767ea4
emadkhan713 <emadkhan713@gmail.com>
2023-08-25 22:24:55 +1000

    ffmpeg npm dependency added, removed redundant download file, documentation WIP

--- linkDownloader.js
+++ linkDownloader.js
@@ -1,6 +1,6 @@
 const yt = require('@googleapis/youtube')
 const { spawn } = require('child_process')
-const { apiKey } = require('./secrets.env')
+const { apiKey } = require('./secrets.json')
 
 // This is a client instance of the YouTube API
 const client = yt.youtube({
@@ -15,9 +15,9 @@ const readline = require('readline').createInterface({
 })
 
 // This prompts the user via the command prompt to enter search terms for the video
-readline.question("Search on YouTube: ", query => {
+readline.question("Search on YouTube: ", userInput => {
     readline.pause()
-    YouTubeQuery(query).then(index => SelectEntry(index))
+    YouTubeQuery(userInput).then(resultList => SelectEntry(resultList))
 })
 
 // A helper function which calls the YouTube API and returns the 5 most relevant results
@@ -53,26 +53,26 @@ async function YouTubeQuery(input) {
     })
 
     // 
-    for (let i = 0; i < videoRes.data.items.length; i++) {
-        vidDurationIso = videoRes.data.items[i].contentDetails.duration
+    for (let i = 0; i < videoResponse.data.items.length; i++) {
+        vidDurationIso = videoResponse.data.items[i].contentDetails.duration
         let vidDurationTiny = require('tinyduration').parse(vidDurationIso)
         let vidDurationSeconds = ((vidDurationTiny.hours ? vidDurationTiny.hours * 3600 : 0) + (vidDurationTiny.minutes ? vidDurationTiny.minutes * 60 : 0) + (vidDurationTiny.seconds ? vidDurationTiny.seconds : 0))
         let vidDuration = new Date(vidDurationSeconds * 1000).toISOString().substring(11, 19)
-        resultIndex[i]['duration'] = vidDuration
+        resultList[i]['duration'] = vidDuration
     }
-    return resultIndex
+    return resultList
 };
 
 // prints out the selections for the 
-function SelectEntry(index) {
-    for (let i = 0; i < index.length; i++) {
+function SelectEntry(resultList) {
+    for (let i = 0; i < resultList.length; i++) {
         console.log(`Option ${i + 1}:`)
-        console.log(index[i])
+        console.log(resultList[i])
     }
     selectSong().then(selection => {
-        let videoSelected = index[selection - 1]
+        let selectedVideo = resultList[selection - 1]
         try {
-            downloadSong(videoSelected.link)
+            downloadSong(selectedVideo.link)
         } catch (err) {
             console.log(err)
         }
@@ -106,7 +106,7 @@ function downloadSong(songUrl) {
         '-o',
         'song.%(ext)s',
         '--ffmpeg-location',
-        './resources'
+        './node_modules./ffmpeg-static/'
     ])
 
     ytdlp.stdout.on('data', (data) => {
