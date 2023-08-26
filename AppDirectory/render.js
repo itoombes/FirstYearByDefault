0eddbdf99bf62695e130d74f7d5a2bb0a21bf824
OliverSparrow <oliversparrowkappiuzhathil@gmail.com>
2023-08-26 16:45:46 +1000

    A quick commit. (Yup, that's about it!)

---
+++ AppDirectory/render.js
@@ -0,0 +1,9 @@
+
+const mediaURL = document.getElementById("link");
+const mediaType = document.getElementById("audioOnly");
+
+function downloadMedia() {
+    if (mediaType.checked) {
+        //
+    }
+}
